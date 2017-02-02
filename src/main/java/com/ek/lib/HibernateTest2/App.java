492e926e95c6a378913dbe492999a3857eb0da24
Ehsan Khatri <EK0080134@techmahindra.com>
2017-02-02 20:15:46 +0530

    first commit

---
+++ src/main/java/com/ek/lib/HibernateTest2/App.java
@@ -0,0 +1,13 @@
+package com.ek.lib.HibernateTest2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
